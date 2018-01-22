82b5f60f08c74510473b1fd6d4a44d412e78306e
Kirill <kirill_nn@inbox.ru>
2018-01-22 11:42:58 +0300

    FilmLibraryDatabaseHelper editing. Part 1

---
+++ app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
@@ -0,0 +1,25 @@
+package com.hfad.filmlibrarydb;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FilmLibraryDatabaseHelper extends SQLiteOpenHelper {
+  private static final String DB_NAME = "film_library";
+  private static final int DB_VERSION = 1;
+  private Context context;
+
+  public FilmLibraryDatabaseHelper(Context context) {
+    super(context, DB_NAME, null, DB_VERSION);
+  }
+
+  @Override
+  public void onCreate(SQLiteDatabase db) {
+
+  }
+
+  @Override
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+  }
+}
