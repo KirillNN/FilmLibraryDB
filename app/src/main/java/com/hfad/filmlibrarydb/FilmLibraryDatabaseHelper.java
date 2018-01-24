1e6569abcbefdf471c3d43248113fca4c7cecaee
Kirill <kirill_nn@inbox.ru>
2018-01-24 12:39:39 +0300

    FilmLibraryDatabaseHelper editing. Part 8

--- app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
+++ app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
@@ -9,10 +9,11 @@ public class FilmLibraryDatabaseHelper extends SQLiteOpenHelper {
 
   private static final String DB_NAME = "film_library";
   private static final int DB_VERSION = 1;
-  private Context context;
+  private Context contextDB;
 
   public FilmLibraryDatabaseHelper(Context context) {
     super(context, DB_NAME, null, DB_VERSION);
+    contextDB = context;
   }
 
   @Override
@@ -35,6 +36,42 @@ public class FilmLibraryDatabaseHelper extends SQLiteOpenHelper {
         + "_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
         + "NAME TEXT, "
         + "DESCRIPTION TEXT);");
+
+    insertGenre(db, contextDB.getString(R.string.action),
+        contextDB.getString(R.string.action_desc));
+
+    insertGenre(db, contextDB.getString(R.string.adventure),
+        contextDB.getString(R.string.adventure_desc));
+
+    insertGenre(db, contextDB.getString(R.string.children),
+        contextDB.getString(R.string.children_desc));
+
+    insertGenre(db, contextDB.getString(R.string.comedy),
+        contextDB.getString(R.string.comedy_desc));
+
+    insertGenre(db, contextDB.getString(R.string.detective),
+        contextDB.getString(R.string.detective_desc));
+
+    insertGenre(db, contextDB.getString(R.string.doc),
+        contextDB.getString(R.string.doc_desc));
+
+    insertGenre(db, contextDB.getString(R.string.drama),
+        contextDB.getString(R.string.drama_desc));
+
+    insertGenre(db, contextDB.getString(R.string.family),
+        contextDB.getString(R.string.family_desc));
+
+    insertGenre(db, contextDB.getString(R.string.fantastic),
+        contextDB.getString(R.string.fantastic_desc));
+
+    insertGenre(db, contextDB.getString(R.string.horror),
+        contextDB.getString(R.string.horror_desc));
+
+    insertGenre(db, contextDB.getString(R.string.thriller),
+        contextDB.getString(R.string.thriller_desc));
+
+    insertGenre(db, contextDB.getString(R.string.western),
+        contextDB.getString(R.string.western_desc));
   }
 
   private static void insertGenre(SQLiteDatabase db, String name, String description) {
