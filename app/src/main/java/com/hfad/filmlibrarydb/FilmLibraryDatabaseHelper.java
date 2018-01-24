1d2a61e69cbc3fec991f3f56a8e9f3ff7a8e4a36
Kirill <kirill_nn@inbox.ru>
2018-01-24 10:35:21 +0300

    FilmLibraryDatabaseHelper editing. Part 2

--- app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
+++ app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class FilmLibraryDatabaseHelper extends SQLiteOpenHelper {
+
   private static final String DB_NAME = "film_library";
   private static final int DB_VERSION = 1;
   private Context context;
@@ -22,4 +23,21 @@ public class FilmLibraryDatabaseHelper extends SQLiteOpenHelper {
   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
   }
+
+  @Override
+  public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    super.onDowngrade(db, oldVersion, newVersion);
+  }
+
+  private void createMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("CREATE TABLE GENRE ("
+        + "_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + "NAME TEXT, "
+        + "DESCRIPTION TEXT);");
+//        + "BONUS TEXT, "
+//        + "TYPE INTEGER, "
+//        + "TYPE_BONUS INTEGER, "
+//        + "ORDER_QUANTITY INTEGER);");
+  }
+
 }
