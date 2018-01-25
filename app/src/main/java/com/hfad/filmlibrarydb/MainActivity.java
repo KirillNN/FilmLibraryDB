aa4f6a692d740430823123899483d9dde1d16ed1
Kirill <kirill_nn@inbox.ru>
2018-01-25 11:16:50 +0300

    FilmLibraryDatabaseHelper editing. Part 13

--- app/src/main/java/com/hfad/filmlibrarydb/MainActivity.java
+++ app/src/main/java/com/hfad/filmlibrarydb/MainActivity.java
@@ -1,12 +1,17 @@
 package com.hfad.filmlibrarydb;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends BaseActivity {
+
+  private static final String TAG = "MainActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Log.d(TAG, "onCreate");
   }
 }
