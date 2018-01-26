1646f0e2955155d7ba11e99eeb6bd75cd93d54e4
Kirill <kirill_nn@inbox.ru>
2018-01-26 09:16:07 +0300

    RecyclerView Part 9

--- app/src/main/java/com/hfad/filmlibrarydb/GenreFragment.java
+++ app/src/main/java/com/hfad/filmlibrarydb/GenreFragment.java
@@ -8,11 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import java.util.ArrayList;
 
 public class GenreFragment extends Fragment {
-
-  private ArrayList genre;
 
   public GenreFragment() {
   }
--- app/src/main/java/com/hfad/filmlibrarydb/MainActivity.java
+++ app/src/main/java/com/hfad/filmlibrarydb/MainActivity.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 public class MainActivity extends BaseActivity {
 
   private static final String TAG = "MainActivity";
-//  static ArrayList genres = new ArrayList();
-  static ArrayList<Item> genres = new ArrayList<>();
-  String genreString01;
-  String genreString02;
+  protected static ArrayList<Item> genres = new ArrayList<>();
+
+  protected String listString01;
+  protected String listString02;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +31,28 @@ public class MainActivity extends BaseActivity {
       fillingGenreList();
     }
     genres.size();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
     db.close();
   }
+
   protected void fillingGenreList() {
     Cursor cursor = db.query("GENRE",
         new String[]{"NAME", "DESCRIPTION"},
         null, null, null, null, null);
 
     if (cursor.moveToFirst()) {
-      genreString01 = cursor.getString(0);
-      genreString02 = cursor.getString(1);
-//        genres.add(genreString01);
-      genres.add(new Item(genreString01, genreString02));
-
-//        genre.add((cursor.getString(0)),(cursor.getString(1));
+      listString01 = cursor.getString(0);
+      listString02 = cursor.getString(1);
+      genres.add(new Item(listString01, listString02));
     }
     while (cursor.moveToNext()) {
-      genreString01 = cursor.getString(0);
-      genreString02 = cursor.getString(1);
-//        genres.add(genreString01);
-      genres.add(new Item(genreString01, genreString02));
+      listString01 = cursor.getString(0);
+      listString02 = cursor.getString(1);
+      genres.add(new Item(listString01, listString02));
     }
     cursor.close();
   }
