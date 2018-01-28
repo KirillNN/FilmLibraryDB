5653a56edfa95906d85c48d813c4a56d646f0d1a
kirill <kirill_nn@inbox.ru>
2018-01-28 18:22:36 +0300

    RecyclerView Part 17

--- app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
+++ app/src/main/java/com/hfad/filmlibrarydb/FilmLibraryDatabaseHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class FilmLibraryDatabaseHelper extends SQLiteOpenHelper {
 
   private static final String DB_NAME = "film_library";
-  private static final int DB_VERSION = 2;
+  private static final int DB_VERSION = 1;
   private Context contextDB;
 
   public FilmLibraryDatabaseHelper(Context context) {
