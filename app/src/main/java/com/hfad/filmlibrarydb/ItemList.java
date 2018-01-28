65d61c9f827a791415a2e98d3a6314e3a2a343c5
kirill <kirill_nn@inbox.ru>
2018-01-28 14:43:03 +0300

    RecyclerView Part 11

--- app/src/main/java/com/hfad/filmlibrarydb/ItemList.java
+++ app/src/main/java/com/hfad/filmlibrarydb/ItemList.java
@@ -2,10 +2,14 @@ package com.hfad.filmlibrarydb;
 
 
 import android.content.Context;
+import android.database.Cursor;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemList extends BaseActivity{
+
+  protected String listString01;
+  protected String listString02;
   private static ItemList ourInstrance = null;
   private List<Item> items;
   private Context context;
@@ -36,6 +40,22 @@ public class ItemList extends BaseActivity{
   }
 
   private void fillingItemList() {
+    Cursor cursor = db.query("GENRE",
+        new String[]{"NAME", "DESCRIPTION"},
+        null, null, null, null, null);
+
+    if (cursor.moveToFirst()) {
+      listString01 = cursor.getString(0);
+      listString02 = cursor.getString(1);
+      items.add(new Item(listString01, listString02));
+    }
+    while (cursor.moveToNext()) {
+      listString01 = cursor.getString(0);
+      listString02 = cursor.getString(1);
+      items.add(new Item(listString01, listString02));
+    }
+    cursor.close();
+    db.close();
   }
 
   private void addItem(Item item) {
--- app/src/main/java/com/hfad/filmlibrarydb/MainActivity.java
+++ app/src/main/java/com/hfad/filmlibrarydb/MainActivity.java
@@ -1,18 +1,13 @@
 package com.hfad.filmlibrarydb;
 
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
-import java.util.ArrayList;
+
 
 
 public class MainActivity extends BaseActivity {
 
   private static final String TAG = "MainActivity";
-  protected static ArrayList<Item> genres = new ArrayList<>();
-
-  protected String listString01;
-  protected String listString02;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,38 +17,4 @@ public class MainActivity extends BaseActivity {
     Log.d(TAG, "onCreate");
   }
 
-  @Override
-  protected void onStart() {
-    super.onStart();
-    Log.d(TAG, "onStart");
-    connectDBRead();
-    if (isConnectedRead) {
-      fillingGenreList();
-    }
-    genres.size();
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    db.close();
-  }
-
-  protected void fillingGenreList() {
-    Cursor cursor = db.query("GENRE",
-        new String[]{"NAME", "DESCRIPTION"},
-        null, null, null, null, null);
-
-    if (cursor.moveToFirst()) {
-      listString01 = cursor.getString(0);
-      listString02 = cursor.getString(1);
-      genres.add(new Item(listString01, listString02));
-    }
-    while (cursor.moveToNext()) {
-      listString01 = cursor.getString(0);
-      listString02 = cursor.getString(1);
-      genres.add(new Item(listString01, listString02));
-    }
-    cursor.close();
-  }
 }
