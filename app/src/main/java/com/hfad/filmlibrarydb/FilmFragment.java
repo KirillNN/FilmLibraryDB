1e8874667558e0e7f04bd4e7772eeb02ac76bb94
Kirill <kirill_nn@inbox.ru>
2018-01-30 09:42:15 +0300

    RecyclerView Part 30

--- app/src/main/java/com/hfad/filmlibrarydb/FilmFragment.java
+++ app/src/main/java/com/hfad/filmlibrarydb/FilmFragment.java
@@ -2,11 +2,17 @@ package com.hfad.filmlibrarydb;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.List;
 
 public class FilmFragment extends Fragment {
+
+  private RecyclerView recyclerView;
+  private List<Film> films;
+//  private FilmAdapter filmAdapter;
 
   public FilmFragment() {
   }
@@ -15,7 +21,6 @@ public class FilmFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_film, container, false);
   }
 
