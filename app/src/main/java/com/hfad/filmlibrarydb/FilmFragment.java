c4d8511d65176cc325d8623baad6046e9c70c682
Kirill <kirill_nn@inbox.ru>
2018-01-30 08:58:01 +0300

    RecyclerView Part 29

---
+++ app/src/main/java/com/hfad/filmlibrarydb/FilmFragment.java
@@ -0,0 +1,22 @@
+package com.hfad.filmlibrarydb;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FilmFragment extends Fragment {
+
+  public FilmFragment() {
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_film, container, false);
+  }
+
+}
